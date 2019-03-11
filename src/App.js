f6fae73a553d48264d8c3c074a14e8e21437657c
dewandaru22 <stevenaji94@gmail.com>
2019-03-11 13:28:20 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+		
+		this.state = {
+      //active: 'Home',
+      name: 'Atta',
+      color: 'Merah',
+      img: 'atta.jpg',
+		};
+	}
+	
+	clicked(menu) {
+		/*this.setState({
+			active: menu,
+    });*/
+    this.setState({
+      name: menu.name,
+      color: menu.color,
+      img: menu.img,
+    });
+  }
+  
+  /*menusaya(){
+    //return <img src={this.state.active+".jpg"} width="200"></img>
+
+    var gambar;
+    if(this.state.active.toLowerCase() === 'merah'){
+      gambar = "satu";
+    }
+    else if(this.state.active.toLowerCase() === 'kuning'){
+      gambar = "tiga";
+    }
+    else if(this.state.active.toLowerCase() === 'hijau'){
+      gambar = "empat";
+    }
+    else if(this.state.active.toLowerCase() === 'biru'){
+      gambar = "dua";
+    }
+    return <img src={gambar+".jpg"} width="200"></img>
+  }*/
+	
+  render() {
+    return (
+	
+		<div id="app">
+			<nav className="nav">{ this.props.items.map(( menu, index) => {
+        var style = 'menu';
+				
+				if (this.state.color === menu.color) {
+					style = `${style} is-active`;
+				}
+				
+				return <a
+					className={style+" "+menu.color}
+					
+					onClick={this.clicked.bind(this,menu)}
+					key={index}
+				>
+					{menu.name}
+				</a>;
+			} ) }
+			</nav>
+			
+			<div className="info ">
+        Ini adalah <span className={"selected "+this.state.color}>
+        {this.state.name}</span><br/><br/>
+
+        <img src={this.state.img} width="200"></img>
+        {/* {this.menusaya()} */} 
+			</div>
+		</div>
+			
+    );
+  }
+}
+
+export default App;
+
---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './Custom.css';
+import App from './App';
+
+const items = [
+    {
+        name    : 'Atta',
+        color   : 'Merah',
+        img     : 'atta.jpg',
+    },
+    {
+        name    : 'Babang',
+        color   : 'Kuning',
+        img     : 'babang.jpg',
+    },
+    {
+        name    : 'Incess',
+        color   : 'Hijau',
+        img     : 'inces.jpg',
+    },
+    {
+        name    : 'Adek',
+        color   : 'Biru',
+        img     : 'dua.jpg',
+    },
+];
+
+ReactDOM.render(<App items={items} />, document.getElementById('root'));
