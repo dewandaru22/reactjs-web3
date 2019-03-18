4c1775240da46c0056263c6bc4c32fac0e5ff180
dewandaru22 <stevenaji94@gmail.com>
2019-03-18 12:38:04 +0700

    Updates

---
+++ precache-manifest.623f5d528f8d588795d327c05f2a8db9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/reactjs-web3/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "70bbb5000cfdab38413d",
+    "url": "/reactjs-web3/static/js/runtime~main.39651cda.js"
+  },
+  {
+    "revision": "a393ecab0282d16cacbc",
+    "url": "/reactjs-web3/static/js/main.6d42bb2d.chunk.js"
+  },
+  {
+    "revision": "cfbda0e9061432ca6501",
+    "url": "/reactjs-web3/static/js/2.f213e47f.chunk.js"
+  },
+  {
+    "revision": "a393ecab0282d16cacbc",
+    "url": "/reactjs-web3/static/css/main.8b24ff1b.chunk.css"
+  },
+  {
+    "revision": "d94829b1f7612b2eff25861eca697222",
+    "url": "/reactjs-web3/index.html"
+  }
+];
